e56f76c4634ea89a6a83b5e413a0b86763a65367
Abhi Patel <abhipatel@Abhis-MacBook-Pro.local>
2025-01-16 18:26:03 -0500

    Lec-2

--- Lecture-2/index.php
+++ Lecture-2/index.php
@@ -15,6 +15,27 @@ session_start();
         print_r($_SESSION);
         session_unset(); // Remove alll session variables
         session_destroy(); // destroy teh session
+        print "<br><br>";
+        $xml = "<Customers>
+    <customer>
+        <id>1</id>
+        <name>Alice</name>
+        <city>New York</city>
+        <Country>USA</Country>
+    </customer>
+    <customer>
+        <id>2</id>
+        <name>Bob</name>
+        <city>Toronto</city>
+        <Country>Canada</Country>
+    </customer>
+</Customers>";
+        $convert_xml = simplexml_load_string($xml);
+        if ($convert_xml === false) {
+            echo "Failed to load XML <br>";
+        } else{
+            print_r($convert_xml);    
+        }
         ?>
 
     </body>
